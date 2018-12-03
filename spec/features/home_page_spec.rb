135bc858bbd41cab1b147e40fa00f4692bb70b29
Justin Dalton <jdalton15@gmail.com>
2018-12-03 12:35:41 -0500

    display ad after every 2 dogs on homepage

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -8,6 +8,7 @@ RSpec.feature "Home page", :type => :feature do
 
     expect(page).to have_selector('.dog-photo', count: 2)
     expect(page).to have_selector('.dog-name', count: 2)
+    expect(page).to have_selector('.ad', count: 1)
 
     expect(page).to have_text("Sign in")
     expect(page).to have_text("Sign up")
