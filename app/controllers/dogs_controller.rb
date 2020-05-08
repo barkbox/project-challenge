e101f5f5516a934b1cd835f759a7044ed479ab00
Elan Kvitko <elan.kvitko@gmail.com>
2020-05-08 09:33:26 -0400

    Add pagination to dogs controller

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -4,7 +4,7 @@ class DogsController < ApplicationController
   # GET /dogs
   # GET /dogs.json
   def index
-    @dogs = Dog.all
+    @dogs = Dog.paginate(page: params[:page])
   end
 
   # GET /dogs/1
