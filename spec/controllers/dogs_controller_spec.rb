3a99040d3c7592d7cb3a797c807b8cc2e3d3622a
skipteel <steel@contently.com>
2020-07-09 18:00:12 -0400

    adds pagination

--- spec/controllers/dogs_controller_spec.rb
+++ spec/controllers/dogs_controller_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe DogsController, type: :controller do
   describe '#index' do
-    it 'displays recent dogs' do
-      2.times { create(:dog) }
+    it 'limits recent dogs to 5' do
+      6.times { create(:dog) }
       get :index
-      expect(assigns(:dogs).size).to eq(2)
+      expect(assigns(:dogs).size).to eq(5)
     end
   end
 end
