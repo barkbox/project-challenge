1a53f66d8a82b916138eed7945b7770d2d023e9c
Pat Holland <pholls@users.noreply.github.com>
2021-02-18 19:21:16 -0800

    add quick and dirty validation

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,4 +3,9 @@ class Dog < ApplicationRecord
   belongs_to :user
 
   alias_method :owner, :user
+
+  validates :name, presence: true
+  validates :owner, presence: true
+  validates :description, presence: true
+  validates :images, presence: true
 end
