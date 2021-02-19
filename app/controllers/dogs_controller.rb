727904d71c08ea41b1ec354167c69de71ec4e626
Pat Holland <pholls@users.noreply.github.com>
2021-02-18 21:44:20 -0800

    remove debug statement

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -90,7 +90,6 @@ class DogsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_dog
-      p params
       @dog = Dog.find(params[:id])
     end
 
