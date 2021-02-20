97f8d34b42b00dcc84ca165af644e258e341bfd9
Pat Holland <pholls@users.noreply.github.com>
2021-02-20 09:15:06 -0800

    login with devise for spec

--- spec/features/dog_resource_spec.rb
+++ spec/features/dog_resource_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
+require_relative '../support/devise'
 
 describe 'Dog resource', type: :feature do
+  let!(:spec_user) { User.first || FactoryBot.create(:user) }
+  before { login_as spec_user }
+
   it 'can create a profile' do
     visit new_dog_path
     fill_in 'Name', with: 'Speck'
@@ -11,7 +15,7 @@ describe 'Dog resource', type: :feature do
   end
 
   it 'can edit a dog profile' do
-    dog = create(:dog)
+    dog = create(:dog, user: spec_user)
     visit edit_dog_path(dog)
     fill_in 'Name', with: 'Speck'
     click_button 'Update Dog'
@@ -19,7 +23,7 @@ describe 'Dog resource', type: :feature do
   end
 
   it 'can delete a dog profile' do
-    dog = create(:dog)
+    dog = create(:dog, user: spec_user)
     visit dog_path(dog)
     click_link "Delete #{dog.name}'s Profile"
     expect(Dog.count).to eq(0)
